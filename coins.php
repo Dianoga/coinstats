a09bb8b1ed334fc290f6f5151dd45023c645dfbc
Brian Steere <dianoga7@3dgo.net>
2014-01-23 12:43:02 -0600

    Grouping sort of works

--- coins.php
+++ coins.php
@@ -1,22 +1,24 @@
 <!doctype html>
 <html ng-app='coinstats'>
 <head>
-	<title>Bitcoin Stats</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js"></script>
+	<title>Coin Stats</title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="//netdna.bootstrapcdn.com/bootswatch/3.0.3/cyborg/bootstrap.min.css" rel="stylesheet">
 	<link rel='stylesheet' href='css/style.css' />
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-	<link rel="icon" href="favicon.ico" type="image/x-icon">
-	<script src='js/stats.js'></script>
 </head>
 <body>
 	<div class='container'>
-		<h1>Bitcoin Stats</h1>
+		<h1>Coin Stats</h1>
 
 		<div class='row' ng-controller="StatsController">
 			<div class='col-md-9'>
-
+				<div class='row' ng-repeat="coinGroup in coinGroups">
+					<div class='coin' ng-repeat="coin in coinGroup">
+						<div class='panel'>{{coin.name}}</div>
+					</div>
+				</div>
 			</div>
 			<div class='list-group col-md-3'>
 				<h4 class='list-group-item active'>Sources</h4>
@@ -25,5 +27,8 @@
 		</div>
 
 	</div>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.min.js"></script>
+	<script src='js/stats.js'></script>
 </body>
 </html>
