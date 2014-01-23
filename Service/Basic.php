f560d4af515589a477846b698a7fedc90fdff1ca
Brian Steere <brian-steere@uiowa.edu>
2014-01-23 12:43:02 -0600

    Grouping sort of works

--- Service/Basic.php
+++ Service/Basic.php
@@ -4,43 +4,43 @@ abstract class BasicService {
 	const ID = '';
 	static protected $cacheDir = '';
 	protected $config;
-	
+
 	public function __construct($config = array()) {
 		$this->config = $config;
 	}
-	
-	abstract public function fetch();	
+
+	abstract public function fetch();
 	abstract protected function process($data);
-	
+
 	public function write_cache($data) {
 		file_put_contents($this->get_cache_path(), serialize($data));
 	}
-	
-	public function read_cache() {		
+
+	public function read_cache() {
 		if(is_file($this->get_cache_path()) && filemtime($this->get_cache_path()) > time() - 5*60) {
 			return unserialize(file_get_contents($this->get_cache_path()));
 		}
 	}
-	
+
 	protected function get_cache_path() {
 		$c = get_called_class();
 		return self::$cacheDir . $c::ID;
-	}	
-	
+	}
+
 	public static function get_data() {
 		$c = get_called_class();
 		return array(
-			'name' => $c::NAME, 
+			'name' => $c::NAME,
 			'id' => $c::ID,
 			'link' => $c::LINK,
 		);
-	}	
-	
+	}
+
 	public static function get_id() {
 		$c = get_called_class();
 		return $c::ID;
 	}
-	
+
 	public static function set_cache_dir($dir) {
 		$dir = rtrim($dir, '/') . '/';
 		self::$cacheDir = $dir;
--- js/stats.js
+++ js/stats.js
@@ -1,8 +1,10 @@
 angular.module('coinstats', [])
-    .controller('StatsController', function($scope, $http, $timeout) {
-        $scope.pools = [];
-        $scope.coins = [];
-        $scope.workers = [];
+    .controller('StatsController', function($scope, $http, $timeout, $filter) {
+        $scope.pools = {};
+        $scope.coins = {};
+        $scope.workers = {};
+
+        $scope.coinGroups = $filter('group')($scope.coins, 3);
 
         $http.get('fetch.php').
         success(function(data) {
@@ -29,6 +31,39 @@ angular.module('coinstats', [])
         };
 
         $scope.process_data = function(data) {
-            console.log(data);
+            angular.forEach(data.balance, function(val, key) {
+                if ($scope.coins[val.type] == undefined) {
+                    $scope.coins[val.type] = {
+                        name: val.type,
+                        balance: 0
+                    };
+                }
+
+                $scope.coins[val.type].balance += parseFloat(val.value);
+            });
+            $scope.coinGroups = $filter('group')($scope.coins, 3);
+
+            angular.forEach(data.workers, function(val, key) {
+                $scope.workers[val.name] = val;
+            });
+        };
+    })
+    .filter('group', function() {
+        return function(items, count) {
+            var newArr = [];
+            var i = 0;
+            angular.forEach(items, function(val, key) {
+                var row = Math.floor(i / count);
+                var col = i % count;
+
+                if (newArr[row] == undefined) {
+                    newArr[row] = [];
+                }
+
+                newArr[row][col] = val;
+                i++;
+            });
+
+            return newArr;
         };
     });
