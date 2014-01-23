5f744c9925e0900f5ae1577c349b176f115f23b8
Brian Steere <dianoga7@3dgo.net>
2014-01-23 10:52:09 -0600

    Add coinex support

---
+++ Service/Coinex.php
@@ -0,0 +1,60 @@
+<?php
+
+class CoinexService extends JsonService {
+	const ID = 'Coinex';
+	const NAME = 'Coinex';
+	const LINK = 'http://coinex.pw';
+	
+	public function __construct($config) {
+		parent::__construct($config);
+		
+		$time = gmdate('Y-m-d\TH:i:s');
+		$id = microtime(true);
+		$vData = "{$this->config['secret']};{$this->config['user']};{$time};{$id};get_balances";
+		$vHash = hash('sha256', $vData);
+		
+		$this->url = "http://coinx.pw/api/v2";
+	}
+	
+	public function fetch() {		
+		//$data = $this->read_cache();
+		if(!$data) {
+			$sign = hash_hmac('sha512', '', $this->config['secret']);
+			$headers = array(
+				'Content-type: application/json',
+				'API-Sign: '.$sign,
+				'API-Key: '.$this->config['apikey'],
+			);
+			
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_URL, 'https://coinex.pw/api/v2/balances');
+			//curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+ 
+			// run the query
+			$result = curl_exec($ch);
+			if($result === false) {
+				return array();
+			}
+			
+			$data = json_decode($result, true);
+			$this->write_cache($data);
+		}
+		
+		return $this->process($data);
+	}
+	
+	protected function process($data) {
+		$clean = array();
+		
+		foreach($data['balances'] as $val) {
+			$clean['balance'][] = array('type' => $val['currency_name'], 'value' => ($val['held'] + $val['amount']) / 100000000);
+		}
+		
+		$clean['workers'] = array();
+		
+		return $clean;
+	}
+}
